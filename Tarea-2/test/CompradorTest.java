9438aff89c454040b7c2d714829ce5d894d3833d
Ricardo <rica14062004@gmail.com>
2023-10-20 17:31:29 -0300

    Creacion de los Test unitarios

---
+++ Tarea-2/test/CompradorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompradorTest {
+
+    @Test
+    void queBebiste() {
+    }
+
+    @Test
+    void getVuelto() {
+    }
+}
---
+++ Tarea-2/test/DepositoTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositoTest {
+
+    @Test
+    void addElemento() {
+    }
+
+    @Test
+    void getElemento() {
+    }
+}
