ec751539b4f64fe6d12a616ab79bc8cefbbe17f3
Ricardo <rica14062004@gmail.com>
2023-10-19 23:01:20 -0300

    Cambio en la utilizacion de depositos, ahora es un Deposito universal

--- Tarea-2/src/Deposito.java
+++ Tarea-2/src/Deposito.java
@@ -6,33 +6,31 @@ import java.util.ArrayList;
  * @author Ricardo Charris
  * @author Vicente Ramirez
  */
-public class DepositoProducto<T> {
-    /**objeto de clase T, para obtener el nombre de la clase en el toString */
-    private T tipoProducto;
+public class Deposito<T> {
     /**guarda varios productos del tipo T */
-    private ArrayList<T> productos;
-    public DepositoProducto(){
-        productos = new ArrayList();
+    private ArrayList<T> elementos;
+    public Deposito(){
+        elementos = new ArrayList<>();
     }
 
     /**
      * Añade un objeto del tipo T al ArrayList de productos
      * @param item objeto que sera añadido al ArrayList productos
      */
-    public void addProducto(T item){
-        productos.add(item);
+    public void addElemento(T item){
+        elementos.add(item);
     }
 
     /**
      * Saca un producto del ArrayList productos y lo devuelve
      * @return objeto de tipo T del ArrayList productos
      */
-    public T getProducto(){
-        if(productos.isEmpty()){
+    public T getElemento(){
+        if(elementos.isEmpty()){
             return null;
         }
         else{
-            return productos.remove(0);
+            return elementos.remove(0);
         }
     }
 
@@ -41,7 +39,7 @@ public class DepositoProducto<T> {
      * @return String que da una descripcion de la clase
      */
     public String toString(){
-        return "\nDeposito que contiene productos de tipo "+tipoProducto.getClass().getSimpleName();
+        return "\nDeposito que contiene objetos de tipo T";
     }
 
 
